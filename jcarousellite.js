f377eec90143715eae25554d12032b7a5de137de
Bryan Chow <bryan@fullfactor.com>
2011-11-15 02:20:03 -0800

    Rename variable 'o' to 'options'

--- jcarousellite.js
+++ jcarousellite.js
@@ -216,9 +216,9 @@
         return el[0].offsetHeight + css(el, 'marginTop') + css(el, 'marginBottom');
     }
 
-    $.fn.jCarouselLite = function(o) {
+    $.fn.jCarouselLite = function(options) {
 
-        o = $.extend({
+        options = $.extend({
             btnPrev: null,
             btnNext: null,
             btnGo: null,
@@ -236,27 +236,35 @@
 
             beforeStart: null,
             afterEnd: null
-        }, o || {});
+        }, options || {});
 
         return this.each(function() { // Returns the element collection. Chainable.
 
-            var running = false, animCss=o.vertical?"top":"left", sizeCss=o.vertical?"height":"width";
-            var div = $(this), ul = $("ul", div), tLi = $("li", ul), tl = tLi.size(), v = o.visible;
+            var running = false,
+                animCss = options.vertical?"top":"left",
+                sizeCss = options.vertical?"height":"width";
 
-            if (o.circular) {
+            var div = $(this),
+                ul = $("ul", div),
+                tLi = $("li", ul),
+                tl = tLi.size(),
+                v = options.visible;
+
+            if (options.circular) {
                 ul.prepend(tLi.slice(tl-v-1+1).clone())
                 .append(tLi.slice(0,v).clone());
-                o.start += v;
+                options.start += v;
             }
 
-            var li = $("li", ul), itemLength = li.size(), curr = o.start;
+            var li = $("li", ul), itemLength = li.size(), curr = options.start;
             div.css("visibility", "visible");
 
-            li.css({overflow: "hidden", float: o.vertical ? "none" : "left"});
+            li.css({overflow: "hidden", float: options.vertical ? "none" : "left"});
             ul.css({margin: "0", padding: "0", position: "relative", "list-style-type": "none", "z-index": "1"});
             div.css({overflow: "hidden", position: "relative", "z-index": "2", left: "0px"});
 
-            var liSize = o.vertical ? height(li) : width(li);   // Full li size(incl margin)-Used for animation
+            // Full li size(incl margin)-Used for animation
+            var liSize = options.vertical ? height(li) : width(li);
             var ulSize = liSize * itemLength;                   // size of full ul(total length, not just for the visible items)
             var divSize = liSize * v;                           // size of entire div(total length for just the visible items)
 
@@ -271,26 +279,26 @@
 
             function go(to) {
                 if (!running) {
-                    if (o.beforeStart) {
-                        o.beforeStart.call(this, vis());
+                    if (options.beforeStart) {
+                        options.beforeStart.call(this, vis());
                     }
                     // If circular we are in first or last, then goto the other
                     // end
-                    if (o.circular) {
+                    if (options.circular) {
                         if (to <= v-1) {
                             // If first, then goto last
                             ul.css(animCss, -((itemLength-(v*2))*liSize)+"px");
                             // If "scroll" > 1, then the "to" might not be
                             // equal to the condition; it can be lesser
                             // depending on the number of elements.
-                            curr = to === v-1 ? itemLength-(v*2)-1 : itemLength-(v*2)-o.scroll;
+                            curr = to === v-1 ? itemLength-(v*2)-1 : itemLength-(v*2)-options.scroll;
                         } else if (to >= itemLength-v+1) {
                             // If last, then goto first
                             ul.css(animCss, -((v) * liSize) + "px");
                             // If "scroll" > 1, then the "to" might not be
                             // equal to the condition; it can be greater
                             // depending on the number of elements.
-                            curr = to === itemLength-v+1 ? v+1 : v+o.scroll;
+                            curr = to === itemLength-v+1 ? v+1 : v+options.scroll;
                         } else {
                             curr = to;
                         }
@@ -307,21 +315,21 @@
                     }
                     running = true;
                     ul.animate(
-                        animCss === "left" ? { left: -(curr*liSize) } : { top: -(curr*liSize) } , o.speed, o.easing,
+                        animCss === "left" ? { left: -(curr*liSize) } : { top: -(curr*liSize) } , options.speed, options.easing,
                         function() {
-                            if (o.afterEnd) {
-                                o.afterEnd.call(this, vis());
+                            if (options.afterEnd) {
+                                options.afterEnd.call(this, vis());
                             }
                             running = false;
                         }
                     );
                     // Disable buttons when the carousel reaches the
                     // last/first, and enable when not
-                    if (!o.circular) {
-                        $(o.btnPrev + "," + o.btnNext).removeClass("disabled");
-                        $((curr-o.scroll<0 && o.btnPrev) ||
-                        (curr+o.scroll > itemLength-v && o.btnNext) ||
-                        []
+                    if (!options.circular) {
+                        $(options.btnPrev + "," + options.btnNext).removeClass("disabled");
+                        $((curr-options.scroll<0 && options.btnPrev) ||
+                            (curr+options.scroll > itemLength-v && options.btnNext) ||
+                            []
                         ).addClass("disabled");
                     }
 
@@ -329,36 +337,36 @@
                 return false;
             }
 
-            if (o.btnPrev) {
-                $(o.btnPrev).click(function() {
-                    return go(curr-o.scroll);
+            if (options.btnPrev) {
+                $(options.btnPrev).click(function() {
+                    return go(curr - options.scroll);
                 });
             }
 
-            if (o.btnNext) {
-                $(o.btnNext).click(function() {
-                    return go(curr+o.scroll);
+            if (options.btnNext) {
+                $(options.btnNext).click(function() {
+                    return go(curr + options.scroll);
                 });
             }
 
-            if (o.btnGo) {
-                $.each(o.btnGo, function(i, val) {
+            if (options.btnGo) {
+                $.each(options.btnGo, function(i, val) {
                     $(val).click(function() {
-                        return go(o.circular ? o.visible+i : i);
+                        return go(options.circular ? options.visible+i : i);
                     });
                 });
             }
 
-            if (o.mouseWheel && div.mousewheel) {
+            if (options.mouseWheel && div.mousewheel) {
                 div.mousewheel(function(e, d) {
-                    return d>0 ? go(curr-o.scroll) : go(curr+o.scroll);
+                    return d>0 ? go(curr - options.scroll) : go(curr + options.scroll);
                 });
             }
 
-            if (o.auto) {
+            if (options.auto) {
                 window.setInterval(function() {
-                    go(curr+o.scroll);
-                }, o.auto+o.speed);
+                    go(curr+options.scroll);
+                }, options.auto + options.speed);
             }
 
         });
